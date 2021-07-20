68b6654557f34ebec71bb30bc9e972cd2d46a1e9
akpinkerton <akpinkerton@gmail.com>
2021-07-19 19:14:38 -0700

    server init

--- app.js
+++ app.js
@@ -1,20 +1,53 @@
 var express = require('express');
+const cors = require("cors");
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var index = require('./routes/index');
 
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cors());
+if (process.env.NODE_ENV !== 'test') {
+  app.use(logger('dev'));
+}
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      'dev error': {
+        message: err.message,
+        error: err
+      }
+    });
+  });
+}
+
+// production error handler no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    'prod error': {
+      message: err.message,
+      error: {}
+    }
+  });
+});
+
 
 module.exports = app;
